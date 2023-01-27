6048f33af0b8beeb100e7eaddb1b7aa555dbe282
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-01-27 12:17:04 +0530

    define() keyword

---
+++ Constant/define.php
@@ -0,0 +1,10 @@
+<?php  
+define("MESSAGE","Hello There !");  
+echo MESSAGE;  
+?> 
+
+<?php    
+define("MSG","Hello message by define keyword",true);//not case sensitive    
+echo MSG, "</br>";    
+echo msg;    
+?>   
