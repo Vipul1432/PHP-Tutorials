3c490f16cadc3cbed4613de2863e0769063e4f1a
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-01-28 23:14:32 +0530

    nowdoc

---
+++ String/nowdoc.php
@@ -0,0 +1,20 @@
+<?php
+
+$input = <<<'testNowdoc'
+
+Welcome to PHP.
+Started coding in PHP!.
+
+testNowdoc;
+
+echo $input;
+
+// Directly printing string without any variable
+echo <<<'Nowdoc'
+<br/>
+Welcome to PHP Course .
+Learning PHP is fun.
+	
+Nowdoc;
+	
+?>
