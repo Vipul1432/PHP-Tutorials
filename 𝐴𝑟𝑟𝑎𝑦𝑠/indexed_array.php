ce9ea69c34538d63ce5ad2cf75f735e2093e0b64
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-01-29 13:38:43 +0530

    indexed_array

---
+++ 𝐴𝑟𝑟𝑎𝑦𝑠/indexed_array.php
@@ -0,0 +1,16 @@
+<!-- first way ; -->
+
+<?php
+$season=array("summer","winter","spring","autumn");  
+echo $season[0];
+?>
+
+<!-- second way ; -->
+
+<?php
+$season[0]="summer";  
+$season[1]="winter";  
+$season[2]="spring";  
+$season[3]="autumn"; 
+echo $season[0];
+?>
