dad395991beb76329532a2f3a0648f78b30c5859
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-02-06 08:07:28 +0530

    Object

---
+++ 𝑂𝑂𝑃𝑠/object/object.php
@@ -0,0 +1,11 @@
+<?php  
+class demo  
+{  
+        private $a= "Hello Mr.Vipul";
+        public function display() {
+        echo $this->a;
+        }
+}  
+    $obj = new demo();
+    $obj->display();
+?>
