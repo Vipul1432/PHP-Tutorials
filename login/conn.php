ef53abc74894a0c205241f644b38825c523fda53
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-02-08 19:54:43 +0530

    login page with database

---
+++ login/conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "127.0.0.1:3307";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// if($conn){
+//     echo "Connected successfully";
+// }
+//     else{
+//         echo "Connection failed";
+//     }
