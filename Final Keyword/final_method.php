efe9b4d07b8bd163b09b595824115cc56442ef96
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-02-10 19:48:21 +0530

    final method

---
+++ Final Keyword/final_method.php
@@ -0,0 +1,33 @@
+<?php
+
+class Base
+{
+    // Final method
+    final public function printdata()
+    {
+        echo " Base class final printdata function";
+    }
+
+    // Non final method
+    public function nonfinal()
+    {
+        echo "\n This is nonfinal function of base class";
+    }
+}
+
+// Class that extend base class
+class Derived extends Base
+{
+    // Inheriting method nonfinal
+    public function nonfinal()
+    {
+        echo "\n Derived class non final function";
+    }
+
+    // Here printdata function can
+    // not be overridden
+}
+
+$obj = new Derived();
+$obj->printdata();
+$obj->nonfinal();
