cc1cf9c56aa8d8f57edc05a85533c011dd30f384
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-02-10 19:40:24 +0530

    Database connection using object oriented

---
+++ Database/mysqli_object_oriented.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "127.0.0.1:3307";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
